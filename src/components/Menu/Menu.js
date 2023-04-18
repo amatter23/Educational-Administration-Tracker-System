ecf2ea55be91848017800f8e24d804c458c018a8
amatter23 <matterahmed36@gmail.com>
2023-04-18 23:35:03 +0200

    Add Menu component

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import classes from './Menu.module.css';
+
+const Menu = props => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default Menu;
