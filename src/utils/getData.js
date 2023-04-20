9cad6bbac023c34b8f2aeeb99338e63ebd86f918
amatter23 <matterahmed36@gmail.com>
2023-04-20 14:52:25 +0200

    Add add user function

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -3,7 +3,7 @@ const auth =
   'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgyMjkxNjg0LCJqdGkiOiJjYTdmYjdiYjBlOTI0MWRkYWI2YThjNGZjOTU4YjlmMyIsInVzZXJfaWQiOjF9.EzTv-RCRnpQ_ZwgW1J4MZCtspiQerIASxWDvwAHVK0M';
 
 export function getUsers() {
-  return fetch('https://fms-production.up.railway.app/auth/users/1/', {
+  return fetch(api_url + '/auth/users/', {
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
       Authorization:
@@ -11,6 +11,42 @@ export function getUsers() {
     },
   })
     .then(response => response.json())
+    .then(data => {
+      return data;
+    });
+}
+
+export function addUser(email, password, userName) {
+  var resp;
+  return fetch(api_url + '/auth/users/', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      Authorization: auth,
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      username: userName,
+      first_name: 'ahmed',
+      last_name: 'mattar',
+    }),
+  })
+    .then(response => {
+      resp = response;
+      return response.json();
+    })
+    .then(json => {
+      return {
+        response: resp,
+        // errorMassage: [json.username, json.email, json.password],
+        // userName: json.username,
+        // email: json.email,
+        // password: json.password,
+        errorMassage: json,
+        error: !resp.ok,
+      };
+    })
     .then(data => {
       return data;
     });
