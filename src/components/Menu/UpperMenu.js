f37b3cc498bf2c4ab0abb7c6543d21a4b0c3366c
amatter23 <matterahmed36@gmail.com>
2023-04-21 10:12:00 +0200

    update menu and response menu

--- src/components/Menu/UpperMenu.js
+++ src/components/Menu/UpperMenu.js
@@ -5,22 +5,81 @@ import {
   faBraille,
   faCircleUser,
   faArrowRightFromBracket,
+  faTableColumns,
+  faAlignJustify,
 } from '@fortawesome/free-solid-svg-icons';
+import { NavLink } from 'react-router-dom';
 const UpperMenu = props => {
+  const [userData, setuserData] = useState({
+    role: 'tracker',
+  });
+  const [menuShow, setmenuShow] = useState(false);
+  const menuItems = () => {
+    if (userData.role === 'tracker') {
+      return (
+        <div className={classes.list}>
+          <ul
+            // style={{
+            //   transform: menuShow ? 'translatex(-110%)' : 'translatex(3%)',
+            // }}
+            className={menuShow? classes.side: ""}
+          >
+            <li>
+              <NavLink
+                to='/'
+                className={({ isActive }) => (isActive ? classes.active : null)}
+                end
+              >
+                <FontAwesomeIcon
+                  className={classes.icon}
+                  icon={faTableColumns}
+                />
+                Dashboard
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to='/trackerForm'
+                className={({ isActive }) => (isActive ? classes.active : null)}
+              >
+                <FontAwesomeIcon
+                  className={classes.icon}
+                  icon={faAlignJustify}
+                />
+                New Form
+              </NavLink>
+            </li>
+          </ul>
+          {/* <div className={classes.footer}>
+            <p>© 2023 All Rights Reserved</p>
+          </div> */}
+        </div>
+      );
+    }
+  };
   return (
     <div className={classes.menu}>
       <div className={classes.menuLogo}>
-        <div className={classes.btn}>
+        <div
+          onClick={() => {
+            setmenuShow(!menuShow);
+          }}
+          className={classes.btn}
+        >
           <FontAwesomeIcon icon={faBraille} className={classes.icon} />
         </div>
-        <div>
-          <FontAwesomeIcon icon={faCircleUser} className={classes.icon}/>
+        <div className={classes.name}>
+          <FontAwesomeIcon icon={faCircleUser} className={classes.icon} />
           <h6>Admin</h6>
         </div>
+        <div  >{menuItems()}</div>
       </div>
 
       <div className={classes.logout}>
-        <FontAwesomeIcon icon={faArrowRightFromBracket} className={classes.icon}/>
+        <FontAwesomeIcon
+          icon={faArrowRightFromBracket}
+          className={classes.icon}
+        />
       </div>
     </div>
   );
