d353e62583103e4b4ce46f20c2af05f9cd7dc96c
amatter23 <matterahmed36@gmail.com>
2023-05-02 09:31:52 +0300

    update user route

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -1,21 +1,18 @@
 import React, { useState, useEffect } from 'react';
-
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import UpperMenu from '../components/Menu/UpperMenu';
-import Menu from '../components/Menu/Menu';
 import classes from './UserRoute.module.css';
 const UserRoute = props => {
+  const [userData, UpdateUserData] = useState(props.userData);
   return (
     <div className={classes.container}>
-      <UpperMenu />
+      <div className={classes.menu}>
+        <UpperMenu userRolesHaveCheckBoxInputs={props.userRolesHaveCheckBoxInputs} userData={userData} />
+      </div>
       <div className={classes.middle}>
-        {/* <div className={classes.menu}>
-        <Menu />
-        </div> */}
         <div className={classes.content}>
-        <Outlet />
+          <Outlet />
         </div>
-       
       </div>
     </div>
   );
