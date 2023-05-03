a11e5879050412f17f5d28f41544bc6e7fddbc2e
amatter23 <matterahmed36@gmail.com>
2023-05-03 08:23:04 +0300

    update loader style

--- src/pages/Loader.js
+++ src/pages/Loader.js
@@ -1,75 +1,10 @@
-import React from "react";
-import { motion } from "framer-motion";
-
-const loadingContainer = {
-  width: "4rem",
-  height: "4rem",
-  display: "flex",
-  justifyContent: "space-around",
-};
-const loadingCircle = {
-  display: "block",
-  width: "1rem",
-  height: "1rem",
-  backgroundColor: "#374e63",
-  borderRadius: "0.5rem",
-};
-
-const loadingContainerVariants = {
-  start: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-  end: {
-    transition: {
-      staggerChildren: 0.2,
-    },
-  },
-};
-
-const loadingCircleVariants = {
-  start: {
-    y: "0%",
-  },
-  end: {
-    y: "60%",
-  },
-};
-const loadingCircleTransition = {
-  duration : 0.4,
-  yoyo : Infinity,
-  ease: 'easeInOut'
-}
-
+import React, { useState } from 'react';
 const Loader = () => {
   return (
-    <div>
-      <div className="fixed  w-full min-h-screen z-50 #374e63 opacity-30" />
-      <div className="flex fixed w-full justify-center items-center h-screen">
-        <motion.div
-          style={loadingContainer}
-          variants={loadingContainerVariants}
-          initial="start"
-          animate="end"
-        >
-          <motion.span
-            style={loadingCircle}
-            variants={loadingCircleVariants}
-            transition={loadingCircleTransition}
-          ></motion.span>
-          <motion.span
-            style={loadingCircle}
-            variants={loadingCircleVariants}
-            transition={loadingCircleTransition}
-          ></motion.span>
-          <motion.span
-            style={loadingCircle}
-            variants={loadingCircleVariants}
-            transition={loadingCircleTransition}
-          ></motion.span>
-        </motion.div>
-      </div>
+    <div className='loader1'>
+      <span className='loader__element'></span>
+      <span className='loader__element'></span>
+      <span className='loader__element'></span>
     </div>
   );
 };
