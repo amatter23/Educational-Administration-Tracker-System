6a09031b6e4636f544559c4cf7753000969f9a9b
amatter23 <matterahmed36@gmail.com>
2023-05-05 23:35:22 +0300

    add all statics to main page

--- src/components/Users/Dashbord/DashbordCheckBox/DashboardCheckBox.js
+++ src/components/Users/Dashbord/DashbordCheckBox/DashboardCheckBox.js
@@ -1,5 +1,5 @@
 //todo add statics to the dashboard
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './DashboardCheckBox.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckSquare, faSquare } from '@fortawesome/free-solid-svg-icons';
@@ -7,8 +7,52 @@ import RowBox from '../BoxStatistics/RowBox/RowBox';
 import UserInformation from '../UserInformationCard/UserInformation';
 import VerticalBarChart from '../VerticalBarChart/VerticalBarChart';
 import PieChart from '../PieChart/PieChart';
-import { getSchools } from '../../../../utils/getData';
+import { getDepartmentStatics } from '../../../../utils/getData';
+import Loader from '../../../../pages/Loader';
 const DashboardCheckBox = props => {
+  // state to handle the loading
+  const [isLoading, setLoading] = useState(true);
+  // state to handle the error
+  const [error, setError] = useState(false);
+  // state to handle the user data
+  const [userData, setUserData] = useState(props.userData);
+  // state to handle the schools data
+  const [schoolStatics, setSchoolStatics] = useState();
+  // state to handle the fields data
+  const [fields, setFields] = useState();
+  // function to fetch school statics
+  const fetchStatics = async () => {
+    setLoading(true);
+    try {
+      const response = getDepartmentStatics(userData.role).then(data => {
+        setLoading(false);
+        if (data.status === 200) {
+          setSchoolStatics(data.result.system_info);
+          setFields(data.result[`${userData.role.replace('_admin', '')}`]);
+          return;
+        } else {
+          setError(true);
+          return;
+        }
+      });
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+    // getData(fields);
+  };
+
+  // useEffect to fetch the data from the server
+  useEffect(() => {
+    fetchStatics();
+  }, []);
+
+  if (isLoading === true) {
+    return <Loader />;
+  } else if (error === true) {
+    return <div>Something went wrong</div>;
+  }
+
   return (
     <div className={classes.container}>
       <div className={classes.top}>
@@ -16,15 +60,15 @@ const DashboardCheckBox = props => {
           <UserInformation userData={props.userData} />
         </div>
         <div className={classes.right}>
-          <RowBox />
+          <RowBox schoolStatics={schoolStatics} />
         </div>
       </div>
       <div className={classes.bottom}>
         <div className={classes.left}>
-          <VerticalBarChart />
+          <VerticalBarChart userData={userData} fields={fields} />
         </div>
         <div className={classes.right}>
-          <PieChart />
+          <PieChart userData={userData} schoolStatics={schoolStatics} />
         </div>
       </div>
     </div>
