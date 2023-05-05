c14dc305af16ab40397915a54b89254f1568a2c3
amatter23 <matterahmed36@gmail.com>
2023-05-05 23:32:31 +0300

    add statics fields

--- src/components/Users/Dashbord/VerticalBarChart/VerticalBarChart.js
+++ src/components/Users/Dashbord/VerticalBarChart/VerticalBarChart.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import classes from './VerticalBarChart.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSquareMinus } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
 
 import {
   Chart as ChartJS,
@@ -23,50 +24,64 @@ ChartJS.register(
   Legend
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top',
-    },
-    title: {
-      display: false,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-};
-
-const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-
-const dataSet1 = [30, 20, 10, 10, 10, 10, 10];
-const dataSet2 = [30, 60, 10, 10, 80, 10, 100];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: 'يوجد مشكله',
-      data: labels.map((label, index) => dataSet1[index]),
-      backgroundColor: '#182938',
-    },
-    {
-      label: 'لا يوجد مشكله',
-      data: labels.map((label, index) => dataSet2[index]),
-      backgroundColor: '#617586',
-    },
-  ],
-};
 const VerticalBarChart = props => {
-  const [formData, setFormData] = useState({
-    issue: 12,
-    noIssue: 19,
-    department: 'الموارد البشرية',
-  });
+  const { t } = useTranslation();
+  const getFieldsName = data => {
+    const keys = Object.keys(data).map(key => key.replace('_admin', ''));
+    var need = [];
+    keys.forEach(element => {
+      // need to loop keys and get keys but remove key is doplcated
+      element = element
+        .replace('_valid', '')
+        .replace('_invalid', '')
+        .replace('_disciplined', '')
+        .replace('_undisciplined', '')
+        .replace('_noexist', '')
+        .replace('_exits', '');
+
+      need.push(element);
+    });
+    return need.filter((item, index) => need.indexOf(item) === index);
+  };
+
+  const keys = Object.values(props.fields);
+  const [arr1, arr2] = keys.reduce(
+    (acc, curr, i) => {
+      acc[i % 2].push(curr);
+      return acc;
+    },
+    [[], []]
+  );
+
+  const dataSet1 = arr2;
+  const dataSet2 = arr1;
+  const [fieldsName, setFieldsName] = useState(getFieldsName(props.fields));
+  const labels = fieldsName.map(label => t(label));
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'يوجد مشكله',
+        data: labels.map((label, index) => dataSet1[index]),
+        backgroundColor: '#182938',
+      },
+      {
+        label: 'لا يوجد مشكله',
+        data: labels.map((label, index) => dataSet2[index]),
+        backgroundColor: '#617586',
+      },
+    ],
+  };
 
   return (
     <div className={classes.container}>
-      <h5>سلبيات قسم {formData.department} </h5>
-      <Bar options={options} data={data} className={classes.datasets} />
+      <h5>
+        {t('Department stats') +
+          ' ' +
+          t(props.userData.role.replace('_admin', ''))}
+      </h5>
+      <Bar data={data} className={classes.datasets} />
     </div>
   );
 };
