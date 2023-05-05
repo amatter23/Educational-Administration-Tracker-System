281c9f8a9758b3a71f63dda9efb98b8c7c06778e
amatter23 <matterahmed36@gmail.com>
2023-05-05 23:31:15 +0300

    add translate

--- src/components/Users/Dashbord/UserInformationCard/UserInformation.js
+++ src/components/Users/Dashbord/UserInformationCard/UserInformation.js
@@ -5,8 +5,11 @@ import classes from './UserInformation.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserPen } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const UserInformation = props => {
+  const { t } = useTranslation();
+
   const [userData, setUserData] = useState(props.userData);
   // state to change the user role to a more readable format
   const userRoleHandler = userData.role
@@ -19,7 +22,7 @@ const UserInformation = props => {
     <div className={classes.container}>
       <div className={classes.content}>
         <h5>{userData.fallName}</h5>
-        <h6>{userRoleHandler}</h6>
+        <h6>{t(userRoleHandler)}</h6>
         <div
           onClick={() => {
             navigate('/userInformation');
