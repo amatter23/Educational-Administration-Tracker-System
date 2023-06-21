c1bcab7181abe8f4d7a9e5366344e1e09635d4c2
Ahmed Matter <matterahmed36@gmail.com>
2023-06-21 22:21:17 +0300

    update a search

--- src/components/Ui/Search_Filter/Search_Filter.js
+++ src/components/Ui/Search_Filter/Search_Filter.js
@@ -6,16 +6,24 @@ import Select from 'react-select';
 import { useTranslation } from 'react-i18next';
 
 const SearchFilter = props => {
-
   //todo handle css styles
   // state to handle the translate
   const { t } = useTranslation();
+
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const handleSearchChange = event => {
+    clearTimeout(searchTimeout);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        props.search(event.target.value, props.role);
+      }, 1000) // Adjust the delay time (in milliseconds) to your desired duration
+    );
+  };
   return (
     <div className={classes.search}>
       <input
-        onChange={event => {
-          props.search(event.target.value, props.role);
-        }}
+        onChange={handleSearchChange}
         type='text'
         placeholder={t('Search')}
         style={{ height: '80%' }}
